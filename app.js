c7d5bb87e66c931da663572073ad3d2415204b0f
talhafarooq <talhaandfarooqgmail.com>
2022-12-30 03:22:13 -0800

    practice switch statement

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 // if else statement
 
-function is_leap_year (year)
+// function is_leap_year (year)
+// {
+//     if(year % 400 == 0)
+//     {
+//         return true
+//     }else if((year % 4) && (year % 100 !=0))
+//     {
+//         return true;
+//     }
+//     else 
+//     {
+//         return false;
+//     }
+// }
+// console.log( is_leap_year(2000) );
+// statement true;
+// console.log(is_leap_year(1990));
+// statement false;
+
+// switch statement 
+
+function day_of_week (day) 
 {
-    if(year % 400 == 0)
-    {
-        return true
-    }else if((year % 4) && (year % 100 !=0))
-    {
-        return true;
-    }
-    else 
-    {
-        return false;
+    switch (day) {
+        case 1 : 
+        console.log("Monday");
+        break;
+        case 2 :
+        console.logn ("Tuesday");
+        break;
+        case 3 :
+        console.log("Wednesday");
+        break;
+        case 4 :
+        console.log("Thursday");
+        break;
+        case 5 :
+        console.log("Friday");
+        break;
+        case 6 :
+        console.log("Saturday");
+        break;
+        case 7 :
+        console.log("Sunday");
+        break;
+        default : "invalid Day Of Week!";
     }
 }
-console.log( is_leap_year(2000) );
-// statement true;
-console.log(is_leap_year(1990));
-// statement false;
+day_of_week(3);
