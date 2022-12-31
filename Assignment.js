fcb6672d8fa6561d98f98cec4bbb4ef3710df010
talhafarooq <talhaandfarooqgmail.com>
2022-12-31 04:07:45 -0800

    added foreach assignment

--- Assignment.js
+++ Assignment.js
@@ -134,11 +134,13 @@ Using the forEach function defined for an array,
 find the sum of the array of numbers. [function add_all(arr) {...}]
 */
 
-function add_all(arr1)
-{
-    var arr1 = [12,2,3,4];
-    arr1.forEach(item => {
-    console.log(item);
-    });
-}
-add_all();
+
+// function add_all(arr) {
+//     let sum = 0;
+//     arr.forEach(item => {
+//         sum += item;
+//         console.log(sum);
+//     });
+// }
+// add_all([12,34,24,21]);
+
