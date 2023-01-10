2786c08797a55b8caa8bdef968eb638f793784de
talhafarooq <talhaandfarooqgmail.com>
2023-01-09 16:49:11 -0800

    added Assignment2.js file

---
+++ Assignment2.js
@@ -0,0 +1,32 @@
+/*
+
+Write a javascript function named reverse which takes a string argument and returns the reversed string.
+*/
+
+// function reverse(_String) {
+//     return _String.split("").reverse("").join(" | ");
+// }
+// console.log(reverse("some value "));
+
+// Given a javascript array of objects having a radius property as shown [{radius: 5}, {radius: 9}, {radius: 2}], write a comparator function to sort it.
+
+
+// function radiusCompare(a,b) {
+//     if(a.radius < b.radius)
+//     {
+//         return -1;
+//     }if(a.radius > b.radius)
+//     {
+//         return 1;
+//     }
+    
+//         return 0;
+//    }
+//    var arr = [{radius: 5}, {radius: 9}, {radius: 2}];
+//    arr.sort(radiusCompare);
+//    console.log(arr);
+
+   
+
+   
+
